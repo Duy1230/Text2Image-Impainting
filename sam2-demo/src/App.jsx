3865b531c009b7c671b8f2e48239784b923353f1
Duy1230 <21522000@gm.uit.edu.vn>
2024-10-15 18:00:08 +0700

    Created Project

---
+++ sam2-demo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Vite + React + Tailwind CSS
+      </h1>
+      <p className="text-lg text-gray-700">
+        Edit <code className="bg-gray-200 p-1 rounded">src/App.jsx</code> and save to test HMR
+      </p>
+    </div>
+  )
+}
+
+export default App
