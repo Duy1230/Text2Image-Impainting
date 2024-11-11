ad70268229f958a5b8cf57afc8e5f017d5273487
Duy1230 <21522000@gm.uit.edu.vn>
2024-11-11 12:30:57 +0700

    GroundingDINO: sent to CUDA and added torch.no_grad()

--- backend/model/groundingDINO.py
+++ backend/model/groundingDINO.py
@@ -12,7 +12,7 @@ TEXT_TRESHOLD = 0.25
 class GroundingDINO:
     def __init__(self):
         self.model = load_model("weights/groundingdino/GroundingDINO_SwinT_OGC.py",
-                                "weights/groundingdino/groundingdino_swint_ogc.pth")
+                                "weights/groundingdino/groundingdino_swint_ogc.pth").to("cuda")
         self.boxes = None
         self.logits = None
         self.phrases = None
@@ -27,27 +27,26 @@ class GroundingDINO:
         )
         image_source = Image.open(BytesIO(image_bytes)).convert("RGB")
         image_transformed, _ = transform(image_source, None)
-        self.image_transformed = image_transformed
+        self.image_transformed = image_transformed.to("cuda")
 
     def predict(self, prompt: str, single_target_mode: bool):
-        boxes, logits, phrases = predict(
-            model=self.model,
-            image=self.image_transformed,
-            caption=prompt,
-            box_threshold=BOX_TRESHOLD,
-            text_threshold=TEXT_TRESHOLD
-        )
-        if single_target_mode:
-            max_idx = logits.argmax()
-            # Keep tensor format with single box
-            self.boxes = boxes[max_idx].unsqueeze(0)
-            # Keep tensor format with single logit
-            self.logits = logits[max_idx].unsqueeze(0)
-            self.phrases = [phrases[max_idx]]  # Phrases is already a list
-        else:
-            self.boxes = boxes
-            self.logits = logits
-            self.phrases = phrases
+        with torch.no_grad():
+            boxes, logits, phrases = predict(
+                model=self.model,
+                image=self.image_transformed,
+                caption=prompt,
+                box_threshold=BOX_TRESHOLD,
+                text_threshold=TEXT_TRESHOLD
+            )
+            if single_target_mode:
+                max_idx = logits.argmax()
+                self.boxes = boxes[max_idx].unsqueeze(0).cpu()
+                self.logits = logits[max_idx].unsqueeze(0).cpu()
+                self.phrases = [phrases[max_idx]]
+            else:
+                self.boxes = boxes.cpu()
+                self.logits = logits.cpu()
+                self.phrases = phrases
 
     def get_boxes(self):
         return self.boxes.tolist()
